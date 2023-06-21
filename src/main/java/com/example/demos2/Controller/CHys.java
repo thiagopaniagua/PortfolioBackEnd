a29270576a33f40ab0d4878b32c3cacbd717bb97
thiagopaniagua <titi.paniaguacae@gmail.com>
2023-06-21 11:53:42 -0300

    enlaces actualizados

--- src/main/java/com/example/demos2/Controller/CHys.java
+++ src/main/java/com/example/demos2/Controller/CHys.java
@@ -28,7 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author titip
  */
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200","https://frontend-d9836.web.app/"})
 @RequestMapping("/skill")
 public class CHys {
     @Autowired
